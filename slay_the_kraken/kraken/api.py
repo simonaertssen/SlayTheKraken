476affb9b40f5acb8c627b7d4da6cddaa1c09258
Simon Aertssen <sa@ticra.com>
2021-12-01 21:26:19 +0100

    Deciding to save this state before simplifying it

--- slay_the_kraken/kraken/api.py
+++ slay_the_kraken/kraken/api.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+import calendar
+import datetime
 import os
 import hmac
 import json
+from typing import Union
 import requests
 
-from time import time_ns
+from time import strptime, time, time_ns
 from hashlib import sha256, sha512
 from urllib.parse import urlencode
 from base64 import b64encode, b64decode
@@ -80,3 +83,37 @@ class API(object):
     def OpenPositions(self) -> dict:
         result: dict = self.private_query('OpenPositions', data={})
         return result
+
+    def OHLC(self, pair: str, interval: int = 0, since: Union[str, int] = 0) -> dict:
+        """Return the open-high-low-close data for a given asset."""
+        # Parse the 'since' as a unix timestamp
+        if isinstance(since, str):
+            since = calendar.timegm(strptime(since))
+            print(since)
+
+        n: int = 720
+        intervals: list[int] = [1, 5, 15, 30, 60, 4*60, 24*60, 7*24*60, 15*24*60]  # in minutes
+        if interval != 0:
+            if interval not in intervals:
+                interval = 1
+
+        if interval == 0 and since == 0:
+            # Default: get the last 720 minutes.
+            interval, since = 1, int(time() - n*interval)
+        elif interval != 0 and since == 0:
+            # Get the last 720 intervals.
+            since = int(time() - n*interval)
+        elif interval == 0 and since != 0:
+            # Get the most appropriate allowed interval and recompute since when.
+            elapsed: int = int(time()) - since
+            def diff(list_value): return abs(list_value - int(elapsed / n))
+            interval = min(intervals, key=diff)
+            since = n*intervals
+        elif interval != 0 and since != 0:
+            pass  # Both are specified by the user
+        else:
+            raise KrakenError(f'Unclear what interval = {interval} and since = {since} are.')
+
+        print(interval, since)
+        # data = {'pair': pair, 'interval': 1}
+        # return self.public_query('OHLC', data=data)
